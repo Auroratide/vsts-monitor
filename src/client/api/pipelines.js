6b9adcf3872c8d905afe6058053cefb08f8bd72f
auroratide <tf.auroratide@gmail.com>
2017-10-11 11:44:37 -0500

    Remove get all; unused now

--- src/client/api/pipelines.js
+++ src/client/api/pipelines.js
@@ -1,14 +1,9 @@
 import http from './http';
 
-const getAll = () => {
-  return http.get('/pipelines').then(res => res.data);
-};
-
 const get = id => {
   return http.get(`/pipelines/${id}`).then(res => res.data);
 };
 
 export default {
-  getAll,
   get
 };
